7438e84351b1560c1735fa8097d4205aabf607c1
Smets Angie <angie.d.n.smets@gmail.com>
2015-07-23 17:52:49 +0200

    sendgryd

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,14 @@
 # Load the Rails application.
+#
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'adns',
+  :password => 'angie1994',
+  :domain => 'http://thawing-everglades-7104.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
