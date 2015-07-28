dfc12724bd1f8b6e52779e7bbbaf9d59a94794d0
Smets Angie <angie.d.n.smets@gmail.com>
2015-07-28 11:21:03 +0200

    modif pull reauest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-
-    # Confirms a logged-in user.
-    def logged_in_user
+    
+  def logged_in_user
       unless logged_in?
         store_location
         flash[:danger] = "Please log in."
